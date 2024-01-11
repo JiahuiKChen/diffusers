27e39d4a0773d8f0f5ada579d4d5e35efbd12a8d
JiahuiKChen <jiahui.k.chen@gmail.com>
2024-01-10 19:53:34 -0600

    simple testing script for StableUnclipImg2Img pipeline

---
+++ image_gen/tests/gen_test.py
@@ -0,0 +1,16 @@
+import os
+import torch
+from diffusers import StableUnCLIPImg2ImgPipeline
+from diffusers.utils import load_image
+
+os.environ['HF_HOME'] = '/home/jc98685/hf_cache' # MIDI Boxes
+
+img_txt_pipe = StableUnCLIPImg2ImgPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-2-1-unclip", torch_dtype=torch.float16, 
+)
+img_txt_pipe.to("cuda:1")
+
+cond_img = load_image("bufo.jpeg")
+prompt = "worried frog"
+gen_image = img_txt_pipe(cond_img, prompt).images[0] 
+gen_image.save("gen_bufo.jpg")
