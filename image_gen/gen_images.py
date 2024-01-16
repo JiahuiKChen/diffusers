0f77ad97ed6ade8a4e3aefd69e742fdf05d051ca
JiahuiKChen <jiahui.k.chen@gmail.com>
2024-01-16 11:41:52 -0600

    Adding A40 box HF cache

--- image_gen/gen_images.py
+++ image_gen/gen_images.py
@@ -8,7 +8,8 @@ from diffusers import StableUnCLIPImg2ImgPipeline
 from diffusers.utils import load_image
 
 ##################################################### SETUP
-os.environ['HF_HOME'] = '/home/jc98685/hf_cache' # MIDI Boxes
+# os.environ['HF_HOME'] = '/home/jc98685/hf_cache' # MIDI Boxes
+os.environ['HF_HOME'] = '/datastor1/jiahuikchen/hf_cache' # CS A40 box
 
 wandb.init(
     project="StableUnclipImageGen",
